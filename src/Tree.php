810fc27c79ade44d7ede6e45c9074b8898e08b6c
witchy <witchycq@163.com>
2021-01-31 13:25:09 +0800

    init

---
+++ src/Tree.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace witchy\phptool;
+
+class Tree
+{
+    /**
+     * 获取分级关系 function
+     *
+     * @param [type] $data  数据源
+     * @param integer $parentId 父级id
+     * @return array  返回数据
+     */
+    function category(&$data, $parentId = 0)
+    {
+        $category = array();
+        foreach ($data as $key => $value) {
+            if ($value['pid'] == $parentId) {
+                unset($data[$key]);
+                $value['child'] = $this->category($data, $value['id']);
+                $category[] = $value;
+            }
+        }
+        return $category;
+    }
+}
